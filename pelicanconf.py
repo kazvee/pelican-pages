da0ded5ff1c1c39c40652412d3c1a3c6ebfac69b
kazvee <kazveedev@gmail.com>
2026-01-10 19:47:42 -0700

    Update config for deploy

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,6 +1,6 @@
 AUTHOR = 'kazvee'
 SITENAME = 'Pelican Pages'
-SITEURL = ""
+SITEURL = "https://kazvee.github.io/pelican-pages"
 
 PATH = "content"
 
@@ -29,5 +29,4 @@ SOCIAL = (
 
 DEFAULT_PAGINATION = 10
 
-# Uncomment following line if you want document-relative URLs when developing
-# RELATIVE_URLS = True
+RELATIVE_URLS = False
